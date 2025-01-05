0b5eb5f7df3770c680a8324c80e65aa2fed56d23
EzelBayraktar (Ömer Faruk DEMİR) <53525132+wEZELBAYRAKTAR@users.noreply.github.com>
2025-01-05 08:52:10 +0300

    Create DNS-MASTER.bat

---
+++ DNS-MASTER.bat
@@ -0,0 +1,9 @@
+@echo off
+ipconfig /flushdns
+ipconfig /release
+ipconfig /renew
+cls
+echo DNS Cache Temizleme yapildi.
+echo Cikis icin herhangi bir tusa basiniz.
+echo wEZELBAYRAKTAR iyi gunler diler.
+pause
